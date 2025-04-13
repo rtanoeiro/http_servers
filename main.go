b08ad8d0615c6c6bda1b4c98a1a3cad45831c18c
rtanoeiro <ramontanoeiro@gmail.com>
2025-04-14 08:32:08 +1000

    added new endpoints

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
 	httpServerMux := http.NewServeMux()
+	// App Endpoint
+	fileServer := http.FileServer(http.Dir("."))
+	httpServerMux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	// Health Endpoint
+	httpServerMux.HandleFunc("/healthz/", healthz)
+
 	httpServer := http.Server{
 		Handler: httpServerMux,
 		Addr:    ":8080",
 	}
-	httpServerMux.Handle("/", http.FileServer(http.Dir(".")))
 	httpServer.ListenAndServe()
+}
+
+func healthz(writer http.ResponseWriter, request *http.Request) {
+	header := writer.Header()
+	header.Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("OK"))
 }
