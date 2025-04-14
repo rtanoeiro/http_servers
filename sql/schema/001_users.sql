0706cd5ef45bc37a503332bff4bc316db341529c
rtanoeiro <ramontanoeiro@gmail.com>
2025-04-14 22:13:13 +1000

    updated users sql

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     id uuid PRIMARY KEY,
     created_at timestamp not null,
     updated_at timestamp not null,
-    email text not null
+    email text unique not null
 );
 
 -- +goose Down
